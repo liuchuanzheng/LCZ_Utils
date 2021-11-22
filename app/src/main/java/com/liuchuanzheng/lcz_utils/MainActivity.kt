ade6a59521ae21e1852bf57114d5b9615881dcbc
liuchuanzheng <1052374416@qq.com>
2021-11-22 18:40:24 +0800

    Initial commit

---
+++ app/src/main/java/com/liuchuanzheng/lcz_utils/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.liuchuanzheng.lcz_utils
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.liuchuanzheng.lib_util.TestUtil
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        TestUtil.test()
+    }
+}
---
+++ lib_util/src/main/java/com/liuchuanzheng/lib_util/TestUtil.kt
@@ -0,0 +1,21 @@
+package com.liuchuanzheng.lib_util
+
+import android.util.Log
+
+/**
+ * <pre>
+ * <img width="128" height="110" src="https://tse3-mm.cn.bing.net/th/id/OIP-C.en-w_oH-yn2UsSRfWnOsGAHaGY?w=198&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7" alt="马刺">
+ * 作者:     <a href="https://github.com/liuchuanzheng">刘传政</a>
+ * 创建时间:  18:17 2021/11/22 0022
+ * QQ:      1052374416
+ * 电话:     18501231486
+ * 描述:
+ * </pre>
+ */
+object TestUtil {
+
+    @JvmStatic
+    fun test() {
+        Log.i("TestUtil","已经成功引入LCZ_Utils库")
+    }
+}
